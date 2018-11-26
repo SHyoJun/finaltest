081b4d9a8c02a3644e15387d11691ef4f07dc49d
wjdals8976 <43737797+wjdals8976@users.noreply.github.com>
2018-11-26 14:30:20 +0900

    Update maze.c

--- maze.c
+++ maze.c
@@ -1,1 +1,17 @@
 #include "maze.h"
+
+
+int getch(){
+  int ch;
+  struct termios buf;
+  struct termios save;
+  tcgetattr(0,&save);
+  buf=save;
+  buf.c_lflag &= ~(ICANON | ECHO);
+  buf.c_cc[VMIN]=1;
+  buf.c_cc[VTIME]=0;
+  tcsetattr(0,TCSAFLUSH,&buf);
+  ch=getchar();
+  tcsetattr(0,TCSAFLUSH,&save);
+  return ch;
+}
