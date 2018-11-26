5e544f9ff10b06678fdd8cc04e28740bba68930f
SHyoJun <42951642+SHyoJun@users.noreply.github.com>
2018-11-26 14:31:18 +0900

    Update maze.c

--- maze.c
+++ maze.c
@@ -15,3 +15,19 @@ int getch(){
   tcsetattr(0,TCSAFLUSH,&save);
   return ch;
 }
+
+void item_switch(){
+  int temp;
+  
+  temp=user->x;
+  user->x=bot->x;
+  bot->x=temp;
+  temp=user->y;
+  user->y=bot->y;
+  bot->y=temp;
+  map[user->y][user->x]=2;
+  map[bot->y][bot->x]=3;
+
+  
+  map_print();
+}
